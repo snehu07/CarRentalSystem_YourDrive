7449184ac117fe5d214a0ffa98cb61ec67a95cb4
Sneha Kumari <cu.18bcs2176@gmail.com>
2022-06-21 09:37:23 +0530

    Created User Interface

---
+++ Destination/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Destination.Areas.Identity.Data;
+using Destination.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Destination.Areas.Identity.IdentityHostingStartup))]
+namespace Destination.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DestinationContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DestinationContextConnection")));
+
+                services.AddDefaultIdentity<DestinationUser>(options => {
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.SignIn.RequireConfirmedAccount = false;
+                })
+                    .AddEntityFrameworkStores<DestinationContext>();
+            });
+        }
+    }
+}
---
+++ Destination/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YourDrive.Models
+{
+    public class Employee
+    {
+        [Key] public int EmployeeId { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")] [Required(ErrorMessage = "This field is required.")] [DisplayName("Full Name")] public string FullName { get; set; }
+
+        [Column(TypeName = "varchar(10)")] [DisplayName("Address")] public string Address { get; set; }
+
+        [Column(TypeName = "varchar(100)")] public string City { get; set; }
+        [Column(TypeName = "nvarchar(250)")] [Required(ErrorMessage = "This field is required.")] [DisplayName("State")] public string State { get; set; }
+        [Column(TypeName = "nvarchar(250)")] [Required(ErrorMessage = "This field is required.")] [DisplayName("Password")] public string Password { get; set; }
+        [Column(TypeName = "nvarchar(250)")] [Required(ErrorMessage = "This field is required.")] [DisplayName("Confirm Password")] public string ConfirmPassword { get; set; }
+        [Column(TypeName = "nvarchar(250)")] [Required(ErrorMessage = "This field is required.")] [DisplayName("Email")] public string Email { get; set; }
+
+
+
+
+    }
+}
